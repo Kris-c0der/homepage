7fd2313a5d91dc96d070ecb9e4414cfbdca11171
Krzysztof Kozak <hithard5@gmail.com>
2020-05-07 21:27:06 +0200

    changed script

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 const h1 = document.querySelector(".hero__title-js");
 const visitor = prompt("Hello. What is your name?");
 
-h1.innerHTML = `Hello, ${visitor}.<br /> I'm glad you're here 🙋‍♂️👋`;
+h1.innerHTML = `Hello, <span>${visitor}</span>.<br /> I'm glad you're here 🙋‍♂️👋`;
