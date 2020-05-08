4e5504ba064137cdc64c6f0514e1f7badc548dc3
Krzysztof Kozak <hithard5@gmail.com>
2020-05-08 13:28:11 +0200

    added if statement to the welcome stript

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 const h1 = document.querySelector(".hero__title-js");
 const visitor = prompt("Hello. What is your name?");
 
-h1.innerHTML = `Hello, <span>${visitor}</span>.<br /> I'm glad you're here 🙋‍♂️👋`;
+if (visitor != null && visitor.length > 0) {
+  h1.innerHTML = `Hello, <span>${visitor}</span>.<br /> I'm glad you're here 🙋‍♂️👋`;
+} else {
+  h1.innerHTML = `Hello, <span>stranger</span>.<br /> I'm glad you're here 🙋‍♂️👋`;
+}
